7941ec43fa1c292b125bdea43780717834e5ffe3
afdolriski <afdolriski@gmail.com>
2019-09-09 23:36:58 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello from gin!")
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+
+	r.Run(":8000")
+}
